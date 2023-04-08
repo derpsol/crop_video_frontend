ff9ba54d9fe2a6079ba118e1a2b59c30b1d1c498
derpsol <franklyjin029@gmail.com>
2023-04-08 02:54:08 -0700

    add play and syncbutton

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useRef, MutableRefObject } from "react";
 import ReactPlayer from "react-player";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 const VideoPlayer = () => {
   const [cropWidth, setCropWidth] = useState(50);
@@ -19,6 +19,25 @@ const VideoPlayer = () => {
   const [zoomHeight, setZoomHeight] = useState(2860);
   const [zoomMT, setZoomMT] = useState(0);
   const [zoomML, setZoomML] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const [played, setPlayed] = useState(0);
+
+  const player1Ref = useRef<ReactPlayer | null>(null);
+  const player2Ref = useRef<ReactPlayer | null>(null);
+
+  const handlePlayPause = () => {
+    setPlaying(!playing);
+  };
+
+  const handleProgress = (state: any) => {
+    setPlayed(state.played);
+  };
+
+  const handleClickSync = () => {
+    if (player1Ref.current && player2Ref.current) {
+      player2Ref.current.seekTo(played);
+    }
+  };
 
   const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
     setCropStartX(event.clientX);
@@ -47,22 +66,22 @@ const VideoPlayer = () => {
     let deltaX = event.clientX - cropStartX;
     let deltaY = event.clientY - cropStartY;
     if (down) {
-      if(deltaY / deltaX > 286 / 500) {
-        deltaX = deltaY * 500 /286;
+      if (deltaY / deltaX > 286 / 500) {
+        deltaX = (deltaY * 500) / 286;
       } else {
-        deltaY = deltaX * 286 / 500;
+        deltaY = (deltaX * 286) / 500;
       }
       setCropWidth(prevWidth + deltaX);
       setCropHeight(prevHeight + deltaY);
       setZoomWidth(250000 / cropWidth);
       setZoomHeight((286 * 286) / cropHeight);
-      setZoomMT(marginTop / cropHeight * 286);
-      setZoomML(marginLeft / cropWidth * 500);
+      setZoomMT((marginTop / cropHeight) * 286);
+      setZoomML((marginLeft / cropWidth) * 500);
     } else if (rightDown) {
       setMarginTop(prevMT + deltaY);
       setMarginLeft(prevML + deltaX);
-      setZoomMT(marginTop / cropHeight * 286);
-      setZoomML(marginLeft / cropWidth * 500);
+      setZoomMT((marginTop / cropHeight) * 286);
+      setZoomML((marginLeft / cropWidth) * 500);
     }
   };
 
@@ -71,45 +90,58 @@ const VideoPlayer = () => {
   };
 
   return (
-    <Box display="flex">
-      <Box
-        position="relative"
-        onContextMenu={handleContextMenu}
-        width="500px"
-        height="286px"
-      >
-        <ReactPlayer
-          url="./video.mp4"
-          playing
-          controls
-          width="100%"
-          height="100%"
-        />
+    <Box mt="20px" display="flex" flexDirection="column" alignItems="center">
+      <Box display="flex">
         <Box
-          onMouseMove={handleMouseMove}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-          position="absolute"
-          top={`${marginTop}px`}
-          left={`${marginLeft}px`}
-          width={cropWidth}
-          height={cropHeight}
-          sx={{
-            border: "2px dotted red",
-            borderRadius: "2px",
-          }}
-        />
-      </Box>
-      <Box ml="20px" width="500px" height="286px" overflow="hidden">
-        <Box ml={`-${zoomML}px`} mt={`-${zoomMT}px`}>
+          position="relative"
+          onContextMenu={handleContextMenu}
+          width="500px"
+          height="286px"
+        >
           <ReactPlayer
+            ref={player1Ref}
             url="./video.mp4"
-            playing
+            playing={playing}
             controls
-            width={zoomWidth}
-            height={zoomHeight}
+            width="100%"
+            height="100%"
+            onProgress={handleProgress}
+          />
+          <Box
+            onMouseMove={handleMouseMove}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            position="absolute"
+            top={`${marginTop}px`}
+            left={`${marginLeft}px`}
+            width={cropWidth}
+            height={cropHeight}
+            sx={{
+              border: "2px dotted red",
+              borderRadius: "2px",
+            }}
           />
         </Box>
+        <Box ml="20px" width="500px" height="286px" overflow="hidden">
+          <Box ml={`-${zoomML}px`} mt={`-${zoomMT}px`}>
+            <ReactPlayer
+              ref={player2Ref}
+              url="./video.mp4"
+              playing={playing}
+              controls
+              width={zoomWidth}
+              height={zoomHeight}
+            />
+          </Box>
+        </Box>
+      </Box>
+      <Box mt="30px">
+        <Button variant="contained" color="success" onClick={handlePlayPause}>
+          {playing ? "Pause" : "Play"}
+        </Button>
+        <Button variant="contained" color="success" onClick={handleClickSync} sx={{ ml: '12px'}}>
+          Sync video 2
+        </Button>
       </Box>
     </Box>
   );
