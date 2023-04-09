ed0a9463a4f74c1181d63fd9287b3ac463df0f93
derpsol <franklyjin029@gmail.com>
2023-04-09 06:43:58 -0700

    add zoom function with mouse wheel

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -5,16 +5,8 @@ import { Box, Button } from "@mui/material";
 const VideoPlayer = () => {
   const [cropWidth, setCropWidth] = useState(50);
   const [cropHeight, setCropHeight] = useState(28);
-  const [cropStartX, setCropStartX] = useState(20);
-  const [cropStartY, setCropStartY] = useState(20);
-  const [down, setDown] = useState(false);
-  const [rightDown, setRightDown] = useState(false);
-  const [prevWidth, setPrevWidth] = useState(0);
-  const [prevHeight, setPrevHeight] = useState(0);
   const [marginTop, setMarginTop] = useState(0);
   const [marginLeft, setMarginLeft] = useState(0);
-  const [prevMT, setPrevMT] = useState(0);
-  const [prevML, setPrevML] = useState(0);
   const [zoomWidth, setZoomWidth] = useState(5000);
   const [zoomHeight, setZoomHeight] = useState(2860);
   const [zoomMT, setZoomMT] = useState(0);
@@ -39,39 +31,28 @@ const VideoPlayer = () => {
     }
   };
 
-  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-    
-  };
+  const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const delta = event.deltaY > 0 ? -10 : 10;
 
-  const handleMouseUp = (event: React.MouseEvent<HTMLDivElement>) => {
-    
+    setCropWidth(Math.max(10, cropWidth + delta));
+    setCropHeight(Math.max(10, cropHeight + (delta * 286) / 500));
+    setZoomWidth(250000 / cropWidth);
+    setZoomHeight((286 * 286) / cropHeight);
+    setZoomMT((marginTop / cropHeight) * 286);
+    setZoomML((marginLeft / cropWidth) * 500);
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    let deltaX = event.clientX - cropStartX;
-    let deltaY = event.clientY - cropStartY;
-    if (down) {
-      if (deltaY / deltaX > 286 / 500) {
-        deltaX = (deltaY * 500) / 286;
-      } else {
-        deltaY = (deltaX * 286) / 500;
-      }
-      setCropWidth(prevWidth + deltaX);
-      setCropHeight(prevHeight + deltaY);
-      setZoomWidth(250000 / cropWidth);
-      setZoomHeight((286 * 286) / cropHeight);
-      setZoomMT((marginTop / cropHeight) * 286);
-      setZoomML((marginLeft / cropWidth) * 500);
-    } else {
-      setMarginTop(deltaY);
-      setMarginLeft(deltaX);
-      setZoomMT((deltaY / cropHeight) * 286);
-      setZoomML((deltaX / cropWidth) * 500);
-    }
-  };
-
-  const handleContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.preventDefault();
+    let deltaX = event.clientX - 20;
+    let deltaY = event.clientY - 20;
+    
+    setMarginTop(deltaY);
+    setMarginLeft(deltaX);
+    setZoomMT((deltaY / cropHeight) * 286);
+    setZoomML((deltaX / cropWidth) * 500);
+    setZoomWidth(250000 / cropWidth);
+    setZoomHeight((286 * 286) / cropHeight);
   };
 
   return (
@@ -79,12 +60,9 @@ const VideoPlayer = () => {
       <Box display="flex" overflow="hidden">
         <Box
           position="relative"
-          onContextMenu={handleContextMenu}
           width="500px"
           height="286px"
           onMouseMove={handleMouseMove}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
         >
           <ReactPlayer
             ref={player1Ref}
@@ -101,6 +79,7 @@ const VideoPlayer = () => {
             left={`${marginLeft}px`}
             width={cropWidth}
             height={cropHeight}
+            onWheel={handleWheel}
             sx={{
               border: "2px dotted red",
               borderRadius: "2px",
@@ -123,14 +102,6 @@ const VideoPlayer = () => {
       <Box mt="30px">
         <Button variant="contained" color="success" onClick={handlePlayPause}>
           {playing ? "Pause" : "Play"}
-        </Button>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={handleClickSync}
-          sx={{ ml: "12px" }}
-        >
-          Sync video 2
         </Button>
       </Box>
     </Box>
